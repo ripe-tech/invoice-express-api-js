65a7c3ed9d1d4b10cd553213e060b673a7a33a7c
Margarida Silva <ms@platforme.com>
2021-12-06 17:59:19 +0000

    feat: get invoice PDF file

--- js/invoice.js
+++ js/invoice.js
@@ -1,10 +1,16 @@
 export const InvoiceAPI = superclass =>
     class extends superclass {
-        async createInvoice(payload, options = {}) {
+        async createInvoice(payload) {
             const url = this.invoicingBaseUrl + "invoices.json";
             const response = await this.post(url, {
                 dataJ: payload
             });
+            return response;
+        }
+
+        async getInvoicePDF(invoiceId, options = {}) {
+            const url = this.invoicingBaseUrl + `api/pdf/${invoiceId}.json`;
+            const response = await this.get(url, options);
             return response;
         }
     };
