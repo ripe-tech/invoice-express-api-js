b108940968a600599af3339459ddb4c2c6c7d7ba
Margarida Silva <ms@platforme.com>
2021-12-09 17:00:49 +0000

    feat: change status of invoice

--- js/invoice.js
+++ js/invoice.js
@@ -8,6 +8,14 @@ export const InvoiceAPI = superclass =>
             return response;
         }
 
+        async changeInvoiceStatus(invoiceId, payload) {
+            const url = this.invoicingBaseUrl + `invoice/${invoiceId}/change-state.json`;
+            const response = await this.put(url, {
+                dataJ: payload
+            });
+            return response;
+        }
+
         async getInvoicePDF(invoiceId, options = {}) {
             const url = this.invoicingBaseUrl + `api/pdf/${invoiceId}.json`;
             const response = await this.get(url, options);
