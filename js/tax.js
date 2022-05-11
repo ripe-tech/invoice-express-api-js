c9059f8aacdc3144aefa54a46c957e0d0c87f159
Margarida Silva <ms@platforme.com>
2022-05-11 16:36:48 +0100

    feat:

--- js/tax.js
+++ js/tax.js
@@ -5,4 +5,13 @@ export const TaxAPI = superclass =>
             const response = await this.get(url, options);
             return response;
         }
+
+        async createTax(payload, options = {}) {
+            const url = this.invoicingBaseUrl + "taxes.json";
+            const response = await this.post(url, {
+                ...options,
+                dataJ: payload
+            });
+            return response;
+        }
     };
